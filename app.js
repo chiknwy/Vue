5d1e1bb339695322e8a81dce2c2ac13e60abccb2
chiknwy <chikogitasatria23@gmail.com>
2023-10-05 22:27:30 +0800

    push

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-// Create a new Vue instance
 new Vue({
-    el: '#app', // This connects to the div with id "app" in your HTML
+    el: '#app', 
     data: {
-        loanAmount: null,   // Initial loan amount
-        interestRate: null, // Initial interest rate
-        loanTerm: null,     // Initial loan term
-        monthlyPayment: null, // Calculated monthly payment
-        calculationHistory: [] // To store the calculation history
+        loanAmount: null,   
+        interestRate: null, 
+        loanTerm: null,     
+        monthlyPayment: null, 
+        calculationHistory: [] 
     },
     methods: {
         calculateMonthlyPayment: function() {
-            // Check if all inputs are provided
+            
             if (this.loanAmount !== null && this.interestRate !== null && this.loanTerm !== null) {
                 const monthlyInterestRate = this.interestRate / 100 / 12;
                 const monthlyPayment = (this.loanAmount * monthlyInterestRate) /
                     (1 - Math.pow(1 + monthlyInterestRate, -this.loanTerm));
                 this.monthlyPayment = monthlyPayment;
 
-                // Add the current calculation to the history
+                
                 this.calculationHistory.push({
                     loanAmount: this.loanAmount,
                     interestRate: this.interestRate,
